88bcb54f72180efd18b71a91cdd791a4ef5cd2e3
Rajalakshmi Nagarajan <200520970@student.georgianc.on.ca>
2022-12-06 23:15:35 -0500

    - checkin for pollution.html and api calls

--- weatherProject/main/views.py
+++ weatherProject/main/views.py
@@ -35,48 +35,48 @@ def index(request):
             "icon": str(list_of_data['weather'][0]['icon']),
         }
 
-        currentWeather = CurrentWeatherStatus(country_code=data['country_code'], dt_in_api=data['dt_in_api'],
-                                              coordinate_x=data['coordinate_x'],
-                                              coordinate_y=data['coordinate_y'], temp=data['temp'],
-                                              pressure=data['pressure'], humidity=data['humidity'], main=data['main'],
-                                              description=data['description'], icon=data['icon'],
-                                              city_name=city.upper())
-        currentWeather.save()
-        responds_data = {"current_weather": currentWeather}
+        current_weather = CurrentWeatherStatus(country_code=data['country_code'], dt_in_api=data['dt_in_api'],
+                                               coordinate_x=data['coordinate_x'],
+                                               coordinate_y=data['coordinate_y'], temp=data['temp'],
+                                               pressure=data['pressure'], humidity=data['humidity'], main=data['main'],
+                                               description=data['description'], icon=data['icon'],
+                                               city_name=city.upper())
+        current_weather.save()
+        responds_data = {"current_weather": current_weather}
 
-        # longitude_of_city = float(list_of_data['coord']['lon'])
-        # latitude_of_city = float(list_of_data['coord']['lat'])
-        # air_quality = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/air_pollution?lat=' +
-        #                                      str(latitude_of_city) + '&lon=' + str(longitude_of_city) +
-        #                                      '&appid=104d5ffac43c1420b0797087139f411c').read()
-        # print(air_quality)
-        # list_of_pollution_data = json.loads(air_quality)
-        #
-        # air_quality_dict = {
-        #     "city_name": city,
-        #     "aqi": str(list_of_pollution_data['list'][0]['main']['aqi']),
-        #     "co": float(list_of_pollution_data['list'][0]['components']['co']),
-        #     "no": float(list_of_pollution_data['list'][0]['components']['no']),
-        #     "no2": float(list_of_pollution_data['list'][0]['components']['no2']),
-        #     "o3": float(list_of_pollution_data['list'][0]['components']['o3']),
-        #     "so2": float(list_of_pollution_data['list'][0]['components']['so2']),
-        #     "pm2_5": float(list_of_pollution_data['list'][0]['components']['pm2_5']),
-        #     "pm10": float(list_of_pollution_data['list'][0]['components']['pm10']),
-        #     "nh3": float(list_of_pollution_data['list'][0]['components']['nh3']),
-        # }
-        # current_air_quality = CurrentAirQualityStatus(city_name=air_quality_dict['city_name'],
-        #                                               aqi=air_quality_dict['aqi'],
-        #                                               co=air_quality_dict['co'],
-        #                                               no=air_quality_dict['no'],
-        #                                               no2=air_quality_dict['no2'],
-        #                                               o3=air_quality_dict['o3'],
-        #                                               so2=air_quality_dict['so2'],
-        #                                               pm2_5=air_quality_dict['pm2_5'],
-        #                                               pm10=air_quality_dict['pm10'],
-        #                                               nh3=air_quality_dict['nh3'])
-        # current_air_quality.save()
-        #
-        # responds_data = {"current_air_quality": current_air_quality}
+        longitude_of_city = float(list_of_data['coord']['lon'])
+        latitude_of_city = float(list_of_data['coord']['lat'])
+        air_quality = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/air_pollution?lat=' +
+                                             str(latitude_of_city) + '&lon=' + str(longitude_of_city) +
+                                             '&appid=104d5ffac43c1420b0797087139f411c').read()
+        print(air_quality)
+        list_of_pollution_data = json.loads(air_quality)
+
+        air_quality_dict = {
+            "city_name": city,
+            "aqi": str(list_of_pollution_data['list'][0]['main']['aqi']),
+            "co": float(list_of_pollution_data['list'][0]['components']['co']),
+            "no": float(list_of_pollution_data['list'][0]['components']['no']),
+            "no2": float(list_of_pollution_data['list'][0]['components']['no2']),
+            "o3": float(list_of_pollution_data['list'][0]['components']['o3']),
+            "so2": float(list_of_pollution_data['list'][0]['components']['so2']),
+            "pm2_5": float(list_of_pollution_data['list'][0]['components']['pm2_5']),
+            "pm10": float(list_of_pollution_data['list'][0]['components']['pm10']),
+            "nh3": float(list_of_pollution_data['list'][0]['components']['nh3']),
+        }
+        current_air_quality = CurrentAirQualityStatus(city_name=air_quality_dict['city_name'],
+                                                      aqi=air_quality_dict['aqi'],
+                                                      co=air_quality_dict['co'],
+                                                      no=air_quality_dict['no'],
+                                                      no2=air_quality_dict['no2'],
+                                                      o3=air_quality_dict['o3'],
+                                                      so2=air_quality_dict['so2'],
+                                                      pm2_5=air_quality_dict['pm2_5'],
+                                                      pm10=air_quality_dict['pm10'],
+                                                      nh3=air_quality_dict['nh3'])
+        current_air_quality.save()
+
+        responds_data = {"current_air_quality": current_air_quality}
         print("end")
 
     else:
@@ -100,18 +100,46 @@ def chart(request):
             break
     print("city array", data_arr)
     print(temp_arr)
-    chart_dict = {
-        "time": data_arr,
-        "temp": temp_arr
-    }
+    # chart_dict = {
+    #     "time": data_arr,
+    #     "temp": temp_arr
+    # }
     # for i, j in data_arr, temp_arr:
     chart_array = [['city_name', 'temperature'], [data_arr[0], temp_arr[0]], [data_arr[1], temp_arr[1]],
-                   [data_arr[2], temp_arr[2]]
-        , [data_arr[3], temp_arr[3]], [data_arr[4], temp_arr[4]], [data_arr[5], temp_arr[5]]]
-    # need to iterate and push to a table in html
+                   [data_arr[2], temp_arr[2]], [data_arr[3], temp_arr[3]], [data_arr[4], temp_arr[4]], [data_arr[5],
+                                                                                                        temp_arr[5]]]
+    # iterate and push to a table in html
     return render(request, "main/chart.html", {'chart_array': json.dumps(chart_array)})
 
 
+def pollution(request):
+    data_fetched = CurrentAirQualityStatus.objects.all()
+    data_arr = []
+    temp_arr = []
+
+    index_1 = 0
+    for i in data_fetched:
+        data_arr.append(i.city_name)
+        temp_arr.append(i.o3)
+        index_1 + 1
+        if index_1 > 5:
+            break
+    print("city array", data_arr)
+    print(temp_arr)
+    # chart_dict = {
+    #     "time": data_arr,
+    #     "temp": temp_arr
+    # }
+    # for i, j in data_arr, temp_arr:
+    pollution_array = [['city_name', 'ozone'], [data_arr[0], temp_arr[0]], [data_arr[1], temp_arr[1]],
+                       [data_arr[2], temp_arr[2]], [data_arr[3], temp_arr[3]], [data_arr[4],
+                                                                                temp_arr[4]],
+                       [data_arr[5], temp_arr[5]]]
+    # iterate and push to a table in html
+    return render(request, "main/pollution.html", {'pollution_array': json.dumps(pollution_array)})
+
+
+# api calls
 def get_all_pollutants(request):
     if request.method == 'GET':
         data_fetched = CurrentAirQualityStatus.objects.all()
