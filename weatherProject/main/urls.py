789b7b3d24d3d2ebc040b14e25665bbf3af5515c
Rajalakshmi Nagarajan <200520970@student.georgianc.on.ca>
2022-12-06 21:45:20 -0500

    - checkin for current weather charts and api

--- weatherProject/main/urls.py
+++ weatherProject/main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('chart/', views.chart),
+    path('pollution/', views.pollution),
+    path('get_by_id', views.get_by_id),
+    path('get_all_elements', views.get_all_elements),
+    path('get_by_id', views.get_by_id),
+    path('get_all_pollutants', views.get_all_pollutants),
 ]
